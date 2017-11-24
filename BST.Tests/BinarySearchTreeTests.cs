4500cc98a63fd3c23d100c72a36979c35db210e7
ivinnichek <vinnichekira@gmail.com>
2017-11-24 23:21:07 +0300

    Add Task13.3

---
+++ BST.Tests/BinarySearchTreeTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BST.Tests
+{
+    [TestFixture]
+    public class BinarySearchTreeTests
+    {
+        #region Tests with String
+        public sealed class CustomStringComparer : IComparer<string>
+        {
+            public int Compare(string x, string y) => x.Length - y.Length;
+        }
+
+        [TestCase]
+        public void Enumerator_String_SortedArray()
+        {
+            string[] array = new string[] { "one", "three", "four" };
+
+            var comparer = new CustomStringComparer();
+
+            var tree = new BinarySearchTree<string>(comparer);
+            tree.Add(array);
+
+            int i = 0;
+            foreach (var items in tree)
+            {
+                array[i++] = items;
+            }
+
+            string[] expectedArr = new string[] { "one", "four", "three" };
+
+            Assert.AreEqual(expectedArr, array);
+        }
+        #endregion
+
+        #region Test with Book
+        sealed class Book : IComparable<Book>, IComparable
+        {
+            public int BookYear { get; set; }
+
+            public int CompareTo(Book book) => this.BookYear - book.BookYear;
+            public int CompareTo(object book) => CompareTo((Book)book);
+        }
+
+        [TestCase]
+        public void Enumerator_Book_SortedArray()
+        {
+            var book1 = new Book() { BookYear = 1897 };
+
+            var book2 = new Book() { BookYear = 1997 };
+
+            var book3 = new Book() { BookYear = 2000 };
+
+
+            var tree = new BinarySearchTree<Book>();
+            tree.Add(book1);
+            tree.Add(book2);
+            tree.Add(book3);
+
+            Book[] resBooks = new Book[3];
+
+            int i = 0;
+            foreach (var item in tree)
+            {
+                resBooks[i++] = item;
+            }
+
+            Book[] expBooks = { book2, book3, book1 };
+
+            Assert.AreEqual(expBooks, resBooks);
+        }
+        #endregion
+
+        #region Test with Struct
+        public struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public sealed class CustomPointComparer : IComparer<Point>
+        {
+            public int Compare(Point x, Point y) => x.X * x.Y - y.X * y.Y;
+        }
+
+        [TestCase]
+        public void Enumerator_Point_SortedArray()
+        {
+            var point1 = new Point() { X = 8, Y = 9 };
+
+            var point2 = new Point() { X = 4, Y = 3 };
+
+            var point3 = new Point() { X = 7, Y = 6 };
+
+            var comparer = new CustomPointComparer();
+
+            var tree = new BinarySearchTree<Point>(comparer.Compare);
+
+            tree.Add(point1);
+            tree.Add(point2);
+            tree.Add(point3);
+
+
+            var resArray = new Point[3];
+
+            int i = 0;
+            foreach (var item in tree)
+            {
+                resArray[i++] = item;
+            }
+
+            Point[] expArr = { point2, point3, point1 };
+
+            Assert.AreEqual(expArr, resArray);
+        }
+        #endregion
+
+        #region Tests with Int
+        [TestCase]
+        public void Enumerator_Int_SortedArray()
+        {
+            int[] array = new int[] { 12, 25, 78, 98, 7, 65, 35 };
+            var tree = new BinarySearchTree<int>();
+            tree.Add(array);
+
+            int i = 0;
+            foreach (var items in tree)
+            {
+                array[i++] = items;
+            }
+
+            int[] expectedArr = new int[] { 7, 12, 25, 35, 65, 78, 98 };
+
+            Assert.AreEqual(expectedArr, array);
+        }
+        #endregion
+    }
+}
---
+++ BST/BinarySearchTree.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BST
+{
+    public sealed class BinarySearchTree<T> : IEnumerable<T>, IEnumerable
+    {
+        #region Private fields
+        private readonly Comparison<T> comparer;
+        private Node<T> root;
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of BinarySearchTree.>
+        /// </summary>
+        public BinarySearchTree() : this((Comparison<T>)null) { }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="BinarySearchTree{T}"/>
+        /// </summary>
+        /// <param name="comparer"> Compares two instances. </param>
+        public BinarySearchTree(IComparer<T> comparer) : this(comparer.Compare) { }
+
+        /// <summary>
+        /// Initialize a new instance of BinarySearchTree."/>
+        /// </summary>
+        public BinarySearchTree(Comparison<T> comparison)
+        {
+            root = null;
+            comparer = comparison ?? Comparer<T>.Default.Compare;
+        }
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Find element in the tree.
+        /// </summary>
+        /// <param name="element"> Element to find. </param>
+        /// <returns> True if the element exists. </returns>
+        public bool Contains(T element) => Contains(root, element);
+
+        /// <summary>
+        /// Clear the tree.
+        /// </summary>
+        public void Clear()
+        {
+            root = null;
+        }
+
+        /// <summary>
+        /// Adds an element in the tree.
+        /// </summary>
+        /// <param name="element"> Element to add. </param>
+        public void Add(T element)
+        {
+            if (ReferenceEquals(element, null)) 
+                throw new ArgumentNullException($"{nameof(element)} is null.");
+
+            root = AddElement(root, element);
+        }
+
+        /// <summary>
+        /// Add collection to the tree.
+        /// </summary>
+        /// <param name="collection"> Collection to add. </param>
+        public void Add(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+                
+        /// <summary>
+        /// Preorder traversal.
+        /// </summary>
+        /// <returns> IEnumerable for preorder. </returns>
+        public IEnumerable<T> PreOrder() => PreOrder(root);
+
+        /// <summary>
+        /// Postorder traversal.
+        /// </summary>
+        /// <returns> IEnumerable for postorder.</returns>
+        public IEnumerable<T> PostOrder() => PostOrder(root);
+
+        /// <summary>
+        /// Inorder traversal.
+        /// </summary>
+        /// <returns> IEnumerable for inorder. </returns>
+        public IEnumerable<T> InOrder() => InOrder(root);
+        #endregion
+
+        #region Iterator
+        /// <summary>
+        /// Iterator.
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InOrder().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        #endregion
+
+        #region Private Methods
+        private Node<T> AddElement(Node<T> node, T x)
+        {
+            if (node == null)
+            {
+                return new Node<T>(x);
+            }
+
+            int temp = comparer(x, node.Value);
+
+            if (temp < 0)
+                node.Left = AddElement(node.Left, x);
+            else if (temp > 0)
+                node.Right = AddElement(node.Right, x);
+
+            return node;
+        }
+
+        private bool Contains(Node<T> node, T element)
+        {
+            if (node == null)
+                return false;
+
+            int temp = comparer(node.Value, element);
+
+            if (temp == 0)
+                return true;
+            else if (temp < 0)
+                return Contains(node.Right, element);
+            else
+                return Contains(node.Left, element);
+        }
+
+        private IEnumerable<T> PreOrder(Node<T> node)
+        {
+            yield return node.Value;
+
+            if (node.Left != null)
+                foreach (var n in PreOrder(node.Left))
+                    yield return n;
+
+            if (node.Right != null)
+                foreach (var n in PreOrder(node.Right))
+                    yield return n;
+        }
+
+        private IEnumerable<T> InOrder(Node<T> node)
+        {
+            if (node.Left != null)
+                foreach (var n in InOrder(node.Left))
+                    yield return n;
+
+            yield return node.Value;
+
+            if (node.Right != null)
+                foreach (var n in InOrder(node.Right))
+                    yield return n;
+        }
+
+        private IEnumerable<T> PostOrder(Node<T> node)
+        {
+            if (node.Left != null)
+                foreach (var n in PostOrder(node.Left))
+                    yield return n;
+
+            if (node.Right != null)
+                foreach (var n in PostOrder(node.Right))
+                    yield return n;
+
+            yield return node.Value;
+        }
+        #endregion
+
+        #region Node
+        internal sealed class Node<T>
+        {
+            public Node<T> Left { get; set; }
+
+            public Node<T> Right { get; set; }
+
+            public T Value { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+        #endregion
+    }
+}
